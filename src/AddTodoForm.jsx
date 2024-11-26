21d707b9fea60b7006b6f10189370afc073ffc75
Ivonne <ipena.alejo@gmail.com>
2024-11-26 00:45:14 -0500

    label fix

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const AddTodoForm = () => {
     return(
         <form>
-            <label htmlFor="todoTitle"></label>
+            <label htmlFor="Title"></label>
             <input type="text" id="todoTitle"/>
             <button type="submit">Add</button>
         </form>
