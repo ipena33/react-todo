95ccb05f9d8420b1a6bc6f92da24dfc34bb6a1ba
Ivonne <ipena.alejo@gmail.com>
2025-01-08 01:49:20 -0500

    lesson 8

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,69 @@ import AddTodoForm from './AddTodoForm';
 
 
 function App() {
-  // const [todoList, setTodoList] = useState(() => {
-  // const savedTodoList = localStorage.getItem("savedTodoList");
-  // return savedTodoList ? JSON.parse(savedTodoList) : [];
-  // });
-
   // state function holds list of todos
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
 
+  const fetchData = async() => {
+    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}}`,
+      }
+    }; 
+
+    console.log(options); 
+    console.log(import.meta.env.VITE_AIRTABLE_API_TOKEN);
+    console.log(import.meta.env.VITE_AIRTABLE_BASE_ID);
+    console.log(import.meta.env.VITE_TABLE_NAME);
+
+
+    try {
+      const response = await fetch(options, url);
+      if (!response.ok) {
+        const message = `Error: ${response.status}`;
+        throw new Error(message);
+      }
+      const data = await JSON.stringify(response.json());
+      // console.log(JSON.stringify(data));
+      console.log(data);
+
+
+      // #################    after I fix 404 error, I will uncomment this block ######################################
+      // const todos = data.records.map((record) => (
+      //   {
+      //     id: record.id,
+      //     name: record.fields.title,
+      // }))
+      // console.log(todos);
+      // setTodoList(todos);
+      // setIsLoading(false);
+      // ####################################################################################################################
+
+
+    } catch (error) {
+      console.log(error.message)
+    }
+  };
+ 
   useEffect(() => {
-    new Promise((resolve, reject) => {
-      // mimic loading delay
-      setTimeout(() => {
-        //calling parameter resolve which is a callback function for when Promise is successful
-        resolve( {data: {todoList : JSON.parse(localStorage.getItem("savedTodoList"))}} ) //pass object with property data as a nested empty object
-      }, 2000);
-    })
-    .then((result) => {
-      setTodoList(result.data.todoList)
-      // turn loading off once data has been fetched
-      setIsLoading(false);
-    });
-  }, []); //empty dependency array
+    fetchData();
+    // new Promise((resolve, reject) => {
+    //   // mimic loading delay
+    //   setTimeout(() => {
+    //     //calling parameter resolve which is a callback function for when Promise is successful
+    //     resolve( {data: {todoList : JSON.parse(localStorage.getItem("savedTodoList"))}} ) //pass object with property data as a nested empty object
+    //   }, 2000);
+    // })
+    // .then((result) => {
+    //   setTodoList(result.data.todoList)
+    //   // turn loading off once data has been fetched
+    //   setIsLoading(false);
+    // });
+  }, []); //empty dependency array (useffect only fires once on each mount)
 
   // save the todoList to localStorage so it stays after refreshing
   useEffect(() => {
